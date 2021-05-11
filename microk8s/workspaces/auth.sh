3c295192d30c41eb40a726ba1599177aae459610
usrbinkat <usrbinkat@braincraft.io>
2021-05-11 11:12:22 -0700

    testing
    Signed-off-by: usrbinkat <usrbinkat@braincraft.io>

---
+++ microk8s/workspaces/auth.sh
@@ -0,0 +1,18 @@
+curl -X POST http://localhost:8001/admins -d 'username=myadmin'
+{
+  "rbac_user": {
+    "comment": "User generated on creation of Admin.",
+    "user_token": "406ee936-4a10-41c0-a9e5-7b9f7b6fc106",
+    "id": "98af4bda-8525-44a6-a745-0c503b146639",
+    "name": "kongadmin",
+    "created_at": 1537966410000,
+    "enabled": true
+  },
+  "consumer": {
+    "created_at": 1537966410000,
+    "id": "94110df6-211a-4f89-b4f3-0a994753ddf9",
+    "status": 0,
+    "username": "admin",
+    "type": 2
+  }
+}
